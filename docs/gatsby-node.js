e3ffc7cba83be81b8ab8e6afa24e08e6493a751c
lokielse <lokielse@gmail.com>
2022-05-10 01:55:55 +0800

    docs(*): enable HMR for docs

--- docs/gatsby-node.js
+++ docs/gatsby-node.js
@@ -4,6 +4,7 @@ const demoLoader = path.resolve('../packages/pattern-demo-loader/cjs/index.js');
 exports.onCreateWebpackConfig = ({ actions, loaders, getConfig }) => {
   const config = getConfig()
 
+  config.resolve.alias['@pattern-ui/core'] = path.resolve('../packages/pattern-core/src')
   config.resolve.alias['@pattern-ui/demos'] = path.resolve('../packages/pattern-demos/src')
 
   config.module.rules = [
