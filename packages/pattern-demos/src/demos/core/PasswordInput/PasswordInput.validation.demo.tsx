a1f982cd45a1e5633d020e83144117b6012664b1
lokielse <lokielse@gmail.com>
2022-05-23 01:48:49 +0800

    feat(password-input): remove input wrapper

--- packages/pattern-demos/src/demos/core/PasswordInput/PasswordInput.validation.demo.tsx
+++ packages/pattern-demos/src/demos/core/PasswordInput/PasswordInput.validation.demo.tsx
@@ -4,14 +4,7 @@ import { PasswordInput } from '@pattern-ui/core';
 export default function Demo() {
   return (
     <div style={{ maxWidth: 320, marginLeft: 'auto', marginRight: 'auto' }}>
-      <PasswordInput label="Your password" placeholder="Your password" error />
-
-      <PasswordInput
-        style={{ marginTop: 15 }}
-        label="Your password"
-        placeholder="Your password"
-        error="Invalid password"
-      />
+      <PasswordInput placeholder="Your password" invalid />
     </div>
   );
 }
